690a3c4c99159d22bb6bad8b336ce0e967a6086e
jmespitiag <jmespitiag@eafit.edu.co>
2023-10-13 15:46:04 -0500

    Initial for sprint 3

---
+++ chat/PruebaVocacional/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Test, Student
+
+admin.site.register(Test)
+admin.site.register(Student )
+
+
---
+++ chat/PruebaVocacional/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+
+class Test(models.Model):
+    
+    nombre = models.CharField(max_length=25)
+    id_estudiante = models.ForeignKey("Student", verbose_name=("ID de estudiante"), on_delete=models.CASCADE)
+    respuesta1 = models.IntegerField(default=00)
+    respuesta2 = models.IntegerField(default=00)
+    respuesta3 = models.IntegerField(default=00)
+    respuesta4 = models.IntegerField(default=00)
+    respuesta5 = models.IntegerField(default=00)
+    respuesta6 = models.IntegerField(default=00)
+    respuesta7 = models.IntegerField(default=00)
+    respuesta8 = models.IntegerField(default=00)
+    respuesta9 = models.IntegerField(default=00)
+    respuesta10 = models.IntegerField(default=00)
+    respuesta11 = models.IntegerField(default=00)
+    respuesta12 = models.IntegerField(default=00)
+    respuesta13  = models.IntegerField(default=00)
+    respuesta14 = models.IntegerField(default=00)
+    respuesta15 = models.IntegerField(default=00)
+    respuesta16 = models.IntegerField(default=00)
+    respuesta17 = models.IntegerField(default=00)
+    respuesta18 = models.IntegerField(default=00)
+    respuesta19 = models.IntegerField(default=00)
+    respuesta20 = models.IntegerField(default=00)
+    respuesta21 = models.IntegerField(default=00)
+    respuesta22 = models.IntegerField(default=00)
+    respuesta23 = models.IntegerField(default=00)
+    respuesta24 = models.IntegerField(default=00)
+    respuesta25 = models.IntegerField(default=00)
+    respuesta26 = models.IntegerField(default=00)
+    respuesta27 = models.IntegerField(default=00)
+    respuesta28 = models.IntegerField(default=00)
+    respuesta29 = models.IntegerField(default=00)
+    respuesta30 = models.IntegerField(default=00)
+    id = models.AutoField(primary_key=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+    
+class Student(models.Model):
+    
+    nombre = models.CharField(max_length=25)
+    id_estudiante = models.AutoField(primary_key=True)
+    
+    def __str__(self):
+        return self.nombre
+    
